8dcbd2e89685767ae15d021a0386756f9eb19316
JamesOkemwa <okemwasjames@gmail.com>
2025-03-04 10:05:25 +0100

    Fetch air quality data data from air quality open data platform

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-import { CESIUM_ION_ACCESS_TOKEN } from './config.js';
+import { CESIUM_ION_ACCESS_TOKEN, AIR_QUALITY_OPEN_DATA_PLATFORM_TOKEN } from './config.js';
+
 // Access Token
 Cesium.Ion.defaultAccessToken = CESIUM_ION_ACCESS_TOKEN;
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
 const viewer = new Cesium.Viewer('cesiumContainer', {
-    globe: false,
-    // skyAtmosphere: new Cesium.skyAtmosphere(),
     sceneModePicker: false,
     baseLayerPicker: false,
-    // geocoder: Cesium.IonGeocodeProviderType.GOOGLE,
+});
+
+viewer.scene.globe.show = false;
+
+// Set the camera to focus on Hamburg
+viewer.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(9.9949553, 53.5542627, 500),
+    orientation: {
+        heading: Cesium.Math.toRadians(0.0),
+        pitch: Cesium.Math.toRadians(-30.0),
+        roll: 0.0
+    },
+    duration: 2 
 });
 
 // Add Google Photorealistic 3D Tiles
@@ -18,3 +29,17 @@ try {
 } catch (error) {
     console.log(error)
 }
+
+// Fetch Air Quality Data from API at 5 seconds intervals
+const fetchAirQuality = async () => {
+    const url = `https://api.waqi.info/feed/geo:53.5458170;9.9746386/?token=${AIR_QUALITY_OPEN_DATA_PLATFORM_TOKEN}`;
+    console.log(url)
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+setInterval(fetchAirQuality, 5000);

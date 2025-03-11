f408d320073f2c8ae4d46fdba67b82390defcab5
JamesOkemwa <okemwasjames@gmail.com>
2025-03-11 10:33:34 +0100

    Add sample config file

---
+++ config.sample.js
@@ -0,0 +1,5 @@
+// config.sample.js - This is a template for the config.js file.
+// Rename this file to config.js and replace the placeholder values with your actual tokens.
+
+export const CESIUM_ION_ACCESS_TOKEN = 'YOUR_CESIUM_ION_ACCESS_TOKEN';
+export const AIR_QUALITY_OPEN_DATA_PLATFORM_TOKEN = 'YOUR_AIR_QUALITY_API_TOKEN';
